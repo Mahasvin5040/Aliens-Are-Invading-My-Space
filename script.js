b16b186633124fd20019d64dd672e32c1a88c58c
GrigoriiAnfalov <grigoriianfalov@gmail.com>
2025-10-25 16:07:57 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-var canvas = document.getElementById("myCanvas");
-var c = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const c = canvas.getContext("2d");
 
-var timeStep = 1.0 / 60.0;
-const img = document.getElementById("imag");
+const timeStep = 1.0 / 60.0;
+const asteroidSprite = document.getElementById("asteroid");
 
 canvas.width = window.innerWidth - 50.0;
 canvas.height = window.innerHeight - 50.0;
 
+
+let asteroidTimer = 0;
+const numOfBalls = 2; // Change this value to change the # of balls
+const ballSize = 50;
+const speed = 200;
+const objects = []; // array for balls
+const asteroidSpawnTime = 2; // in seconds
+
+
+function createObject(collisionFlag, sprite, size, posx, posy, velx, vely){
+    this.collisionFlag = collisionFlag;
+    this.sprite = sprite;
+    this.size = size;
+    this.pos = {x: posx, y: posy};
+    this.vel = {x:velx, y:vely};
+}
+
+//Creates balls and populates array
+function createAsteroids(){
+    for(let i = 0; i < numOfBalls; i++){
+        //creates the object
+        const object = new createObject(1, asteroidSprite, ballSize, getRandomInt(0, canvas.width), -ballSize, getRandomInt(-speed, speed), 50);
+        objects.push(object);
+    }
+}
+
+let arrLength = numOfBalls;
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -17,141 +44,126 @@ function getRandomInt(min, max) {
 function draw(){
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    for(var i = 0; i < numOfBalls; i++){
-        obj = balls[i];
-        c.fillStyle = obj.color;
+    for(let i = 0; i < arrLength; i++){
+        obj = objects[i];
 
-
+        /*
+        c.fillStyle = "red";
         c.beginPath();
         c.rect(obj.pos.x, obj.pos.y, obj.size, obj.size);
         c.closePath();
         c.fill();
+        */
+        c.drawImage(obj.sprite, obj.pos.x, obj.pos.y, obj.size, obj.size);
 
-        //c.drawImage(img, obj.pos.x, obj.pos.y, obj.size, obj.size);
     }
+}
+
+function update(){
+
+    if(asteroidTimer > 60 * asteroidSpawnTime){
+        createAsteroids();
+        asteroidTimer = 0;
+    }
+
+    calculate();
+    collision();
+    draw();
+    requestAnimationFrame(update);
+
+    arrLength = objects.length;
+    asteroidTimer++;
+
 }
 
 //updates the position of the ball
-function calculate(ball){
-    ball.pos.x += ball.vel.x * timeStep;
-    ball.pos.y += ball.vel.y * timeStep;
-    var UBX = canvas.width - ball.size;
-    var LBX = 0;
+function calculate(){
+    for(let i = 0; i < arrLength; i++){
+        let ball = objects[i];
 
-    var UBY = canvas.height - ball.size;
-    var LBY = 0;
+        ball.pos.x += ball.vel.x * timeStep;
+        ball.pos.y += ball.vel.y * timeStep;
+        let UBX = canvas.width - ball.size;
+        let LBX = 0;
 
-    if(ball.pos.x > UBX){
-        ball.pos.x = UBX;
-        ball.vel.x *= -1.0;
+        let UBY = canvas.height - ball.size;
+        let LBY = -ballSize;
+
+        if(ball.pos.x > UBX){
+            ball.pos.x = UBX;
+            ball.vel.x *= -1.0;
+        }
+
+        if(ball.pos.x < LBX){
+            ball.pos.x = LBX;
+            ball.vel.x *= -1.0;
+        }
+
+        if(ball.pos.y > canvas.height){
+            objects.splice(i,1);
+            arrLength--;
+        }
+
+        if(ball.pos.y < LBY){
+            ball.pos.y = LBY;
+            ball.vel.y *= -1.0;
+        }
     }
 
-    if(ball.pos.x < LBX){
-        ball.pos.x = LBX;
-        ball.vel.x *= -1.0;
-    }
 
-    if(ball.pos.y > UBY){
-        ball.pos.y = UBY;
-        ball.vel.y *= -1.0;
-    }
-
-    if(ball.pos.y < LBY){
-        ball.pos.y = LBY;
-        ball.vel.y *= -1.0;
-    }
 }
 
 function collision(){
-    for(var i = 0; i < numOfBalls -1; i++){
-        for(var j = i+1; j < numOfBalls; j++){
-            let ball1 = balls[i];
-            let ball2 = balls[j];
+    for(let i = 0; i < arrLength -1; i++){
+        for(let j = i+1; j < arrLength; j++){
+            let ball1 = objects[i];
+            let ball2 = objects[j];
 
             let distx1 = ball1.pos.x - ball2.pos.x;
             let disty1 = ball1.pos.y - ball2.pos.y;
 
-            if(Math.abs(distx1) < ballSize && Math.abs(disty1) < ballSize){
-                if(Math.abs(distx1) < ballSize && Math.abs(distx1) > Math.abs(disty1)){
-                    //console.log("x");
-                    ball1.vel.x *= -1;
-                    ball2.vel.x *= -1;
+            if(ball1.collisionFlag == 1 && ball2.collisionFlag==1){
+                if(Math.abs(distx1) < ballSize && Math.abs(disty1) < ballSize){
+                    if(Math.abs(distx1) < ballSize && Math.abs(distx1) > Math.abs(disty1)){
+                        //console.log("x");
+                        ball1.vel.x *= -1;
+                        ball2.vel.x *= -1;
 
-                    if(distx1 < 0){
-                        let cent = ball1.pos.x + Math.abs(distx1/2.0);
-                        //console.log(distx1, cent );
-                        ball1.pos.x = cent - ballSize/2.0;
-                        ball2.pos.x = cent + ballSize/2.0;
-                    }else if(distx1 > 0){
-                        let cent = ball2.pos.x + Math.abs(distx1/2.0);
-                        //console.log(distx1, cent );
-                        ball1.pos.x = cent + ballSize/2.0;
-                        ball2.pos.x = cent - ballSize/2.0;
+                        if(distx1 < 0){
+                            let cent = ball1.pos.x + Math.abs(distx1/2.0);
+                            //console.log(distx1, cent );
+                            ball1.pos.x = cent - ballSize/2.0;
+                            ball2.pos.x = cent + ballSize/2.0;
+                        }else if(distx1 > 0){
+                            let cent = ball2.pos.x + Math.abs(distx1/2.0);
+                            //console.log(distx1, cent );
+                            ball1.pos.x = cent + ballSize/2.0;
+                            ball2.pos.x = cent - ballSize/2.0;
+                        }
+                    }
+                    else if(Math.abs(disty1) < ballSize && Math.abs(distx1) < Math.abs(disty1)){
+                        //console.log("y");
+                        ball1.vel.y *= -1;
+                        ball2.vel.y *= -1;
+
+                        if(disty1 < 0){
+                            let cent = ball1.pos.y + Math.abs(disty1/2.0);
+                            //console.log(disty1, cent );
+                            ball1.pos.y = cent - ballSize/2.0;
+                            ball2.pos.y = cent + ballSize/2.0;
+                        }else if(disty1 > 0){
+                            let cent = ball2.pos.y + Math.abs(disty1/2.0);
+                            //console.log(disty1, cent );
+                            ball1.pos.y = cent + ballSize/2.0;
+                            ball2.pos.y = cent - ballSize/2.0;
+                        }
                     }
                 }
-                else if(Math.abs(disty1) < ballSize && Math.abs(distx1) < Math.abs(disty1)){
-                    //console.log("y");
-                    ball1.vel.y *= -1;
-                    ball2.vel.y *= -1;
 
-                    if(disty1 < 0){
-                        let cent = ball1.pos.y + Math.abs(disty1/2.0);
-                        //console.log(disty1, cent );
-                        ball1.pos.y = cent - ballSize/2.0;
-                        ball2.pos.y = cent + ballSize/2.0;
-                    }else if(disty1 > 0){
-                        let cent = ball2.pos.y + Math.abs(disty1/2.0);
-                        //console.log(disty1, cent );
-                        ball1.pos.y = cent + ballSize/2.0;
-                        ball2.pos.y = cent - ballSize/2.0;
-                    }
-                }
             }
         }
     }
 }
 
-function update(){
-    for(var i = 0; i < numOfBalls; i++){
-        calculate(balls[i]);
-    }
-    collision();
-    draw();
-    requestAnimationFrame(update);
-}
-
-const numOfBalls = 10; // Change this value to change the # of balls
-const ballSize = 40;
-const speed = 500;
-const balls = []; // array for balls
-//Creates balls and populates array
-for(var i = 0; i < numOfBalls; i++){
-
-    //Creates 3 new colors
-
-    var r = Math.floor(Math.random() * 206 + 50);
-    var g = Math.floor(Math.random() * 206 + 50);
-    var b = Math.floor(Math.random() * 206 + 50);
-
-    //sets a random color to 0 for more vibrant colors
-    var rgb = getRandomInt(1, 4);
-    if(rgb == 1){
-        r = 0;
-    }else if(rgb == 2){
-        g = 0;
-    }else{
-        b = 0;
-    }
-    const randomColor = `rgb(${r}, ${g}, ${b})`;
-
-    //creates the object
-    const object = {
-        color : randomColor,
-        size : ballSize, // change this value to change the size of the balls
-        pos : {x : getRandomInt(0, canvas.width), y : getRandomInt(0, canvas.height)},
-        vel : {x : getRandomInt(-speed, speed), y : getRandomInt(-speed, speed)},
-    }
-    balls.push(object);
-}
-
+createAsteroids();
 update();
